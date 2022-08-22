2f60ded2a9d31b0241360ba7f2872209a5dcddfb
Soroush Kaviani <90821955+srkaviani@users.noreply.github.com>
2022-08-22 15:19:12 +0430

    Create generate-certs.sh

---
+++ generate-certs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Generate certificates for your OpenSearch cluster
+
+OPENDISTRO_DN="/C=IR/ST=KH/L=MASHHAD/O=EXAMPLE"   # Edit here and in opensearch.yml
+
+mkdir -p certs/{ca,os-dashboards}
+
+# Root CA
+openssl genrsa -out certs/ca/ca.key 2048
+openssl req -new -x509 -sha256 -days 1095 -subj "$OPENDISTRO_DN/CN=CA" -key certs/ca/ca.key -out certs/ca/ca.pem
+
+# Admin
+openssl genrsa -out certs/ca/admin-temp.key 2048
+openssl pkcs8 -inform PEM -outform PEM -in certs/ca/admin-temp.key -topk8 -nocrypt -v1 PBE-SHA1-3DES -out certs/ca/admin.key
+openssl req -new -subj "$OPENDISTRO_DN/CN=ADMIN" -key certs/ca/admin.key -out certs/ca/admin.csr
+openssl x509 -req -in certs/ca/admin.csr -CA certs/ca/ca.pem -CAkey certs/ca/ca.key -CAcreateserial -sha256 -out certs/ca/admin.pem
+
+# OpenSearch Dashboards
+openssl genrsa -out certs/os-dashboards/os-dashboards-temp.key 2048
+openssl pkcs8 -inform PEM -outform PEM -in certs/os-dashboards/os-dashboards-temp.key -topk8 -nocrypt -v1 PBE-SHA1-3DES -out certs/os-dashboards/os-dashboards.key
+openssl req -new -subj "$OPENDISTRO_DN/CN=os-dashboards" -key certs/os-dashboards/os-dashboards.key -out certs/os-dashboards/os-dashboards.csr
+openssl x509 -req -in certs/os-dashboards/os-dashboards.csr -CA certs/ca/ca.pem -CAkey certs/ca/ca.key -CAcreateserial -sha256 -out certs/os-dashboards/os-dashboards.pem
+rm certs/os-dashboards/os-dashboards-temp.key certs/os-dashboards/os-dashboards.csr
+
+# Nodes
+for NODE_NAME in "os01" "os02" "os03"
+do
+    mkdir "certs/${NODE_NAME}"
+    openssl genrsa -out "certs/$NODE_NAME/$NODE_NAME-temp.key" 2048
+    openssl pkcs8 -inform PEM -outform PEM -in "certs/$NODE_NAME/$NODE_NAME-temp.key" -topk8 -nocrypt -v1 PBE-SHA1-3DES -out "certs/$NODE_NAME/$NODE_NAME.key"
+    openssl req -new -subj "$OPENDISTRO_DN/CN=$NODE_NAME" -key "certs/$NODE_NAME/$NODE_NAME.key" -out "certs/$NODE_NAME/$NODE_NAME.csr"
+    openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost,IP:127.0.0.1,DNS:$NODE_NAME") -in "certs/$NODE_NAME/$NODE_NAME.csr" -CA certs/ca/ca.pem -CAkey certs/ca/ca.key -CAcreateserial -sha256 -out "certs/$NODE_NAME/$NODE_NAME.pem"
+    rm "certs/$NODE_NAME/$NODE_NAME-temp.key" "certs/$NODE_NAME/$NODE_NAME.csr"
+done
